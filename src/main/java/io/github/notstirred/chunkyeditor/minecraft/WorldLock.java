611d4e4d154e5e164ae9b66be61d0e89f88243cf
Tom Martin <tom.martin1239@gmail.com>
2023-01-15 02:33:09 +0000

    Add javadoc to WorldLock

--- src/main/java/io/github/notstirred/chunkyeditor/minecraft/WorldLock.java
+++ src/main/java/io/github/notstirred/chunkyeditor/minecraft/WorldLock.java
@@ -11,6 +11,11 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.function.Function;
 
+/**
+ * This class isn't really a lock at all, but it first asks the user if they are sure they wish to modify their world.
+ * If they say yes, the world lockfile timestamp is recorded.
+ * If that ever changes the user is <u>bombarded</u> with additional LARGER warnings.
+ */
 public class WorldLock {
     @NotNull
     private final File lockFile;
